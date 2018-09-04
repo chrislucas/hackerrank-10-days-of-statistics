50d6391879c3e210d3242cad72de2f94db4ce19d
chrislucas <christoffer.luccas@gmail.com>
2018-09-04 16:54:33 -0300

    estudos sobre distribuicao normal, funcao de distribuicao de probabilidade da distribuicao normal e a funcao de erro

---
+++ python/GeometricDistributionI/solution/GeoDistribI.py
@@ -0,0 +1,9 @@
+'''
+https://www.hackerrank.com/challenges/s10-geometric-distribution-1/problem
+https://en.wikipedia.org/wiki/Negative_binomial_distribution
+'''
+
+
+
+if __name__ == '__main__':
+    pass
--- python/NormalDistributionI/solution/NormalDistribution.py
+++ python/NormalDistributionI/solution/NormalDistribution.py
@@ -2,11 +2,19 @@
 https://www.hackerrank.com/challenges/s10-normal-distribution-1/problem
 '''
 
+# https://en.wikipedia.org/wiki/Normal_distribution
+# https://en.wikipedia.org/wiki/Cumulative_distribution_function
+# https://se.mathworks.com/help/matlab/ref/erf.html
+from math import erf
+
+def cdf(x, mean, stddev):
+    return .5 * (1 + erf((x - mean) / (stddev * 2 ** .5)))
 
 def run():
     mean, stddev = tuple(map(int, input().split(" ")))
-    #less = float(input())
-    a, b = tuple(map(int, input().split(" ")))
+    less = float(input())
+    print("%.3f" % cdf(less, mean, stddev))
+    a, b = tuple(map(float, input().split(" ")))
     '''
         a equacao da curva normal utiliza 2 parâmetra: a media e o desvio padrao
         Denota-se N(m, dp) a curva normal com media 'm' e desvio padrao 'dp'
@@ -14,10 +22,10 @@ def run():
         A distribuicao normal é simétrica em torno da média o que implica que a media,
         a mediana e a moda sao iguais
     '''
-    z1 = 1 - (abs(a - mean) / stddev)
-    z2 = 1 - (abs(b - mean) / stddev)
+    # z1 = ((a - mean) / stddev)
+    # z2 = ((b - mean) / stddev)
+    print("%.3f" % (cdf(b, mean, stddev) - cdf(a, mean, stddev)))
 
-    print(z1 + z2)
 
 run()
 
